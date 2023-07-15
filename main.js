1878c0ac34d6fb5cf1571e922d445d77bbb19ada
mogamoga <mogamoga.g1thu6@gmail.com>
2023-07-15 14:31:19 +0900

    a

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ const App = {
         return {
             HIT: HIT, HIT_BASE: HIT_BASE, HIT_ENDLESS: HIT_ENDLESS,
             originalMasekiList: masekiList,
-            masekiList: masekiList
+            masekiList: masekiList,
+            effectList: [],
         }
+    },
+    created() {
+        const tmpEffectList = this.originalMasekiList.map(e => {
+            return {value: e.enchantList[0].name, display: e.enchantList.map(f => f.name).join("/")}
+        });
+        this.effectList = [{value: "all", display: "ALL"}].concat(tmpEffectList);
     },
     methods: {
         onChangeAttr(e) {
