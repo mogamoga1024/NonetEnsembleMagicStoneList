8d21df2591cc64760422b8a4d05e2d6ddf9c7769
mogamoga <mogamoga.g1thu6@gmail.com>
2023-07-15 13:49:34 +0900

    騙しあうなんて

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ const App = {
     data() {
         return {
             HIT: HIT, HIT_BASE: HIT_BASE, HIT_ENDLESS: HIT_ENDLESS,
-            maeskiList: masekiList
+            originalMasekiList: masekiList,
+            masekiList: masekiList
         }
     },
     methods: {
-
+        onChangeAttr(e) {
+            const attr = e.target.value;
+            if (attr === "all") {
+                this.masekiList = this.originalMasekiList;
+            }
+            else {
+                this.masekiList = this.originalMasekiList.filter(e => e.attrList.includes(attr));
+            }
+        },
     }
 };
 
